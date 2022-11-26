ebd661a7baa97efdedf81ace397f05715d2af649
BobaFett1219 <46388019+DarthBaguette@users.noreply.github.com>
2022-11-26 15:13:46 -0800

    Update

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,6 @@
 from sympy import *
 
-t = symbols('t')
-res = integrate(-20*sin(t)*cos(t) - 16*sin(t)**2 + 8*cos(t)**2, (t, 0, pi/4))
-print(res)
+x = symbols('x')
 
+res = integrate(cos(x) * cos(3*x))
+print(res)
