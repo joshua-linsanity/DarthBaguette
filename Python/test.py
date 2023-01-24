4f6883d48a231626e86df1e4c0a63299de671cf7
DarthBaguette <jlin5438@outlook.com>
2023-01-23 23:11:59 -0800

    Updated test.py with Week 2 Homework

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,4 @@
-from sympy import *
-
-t = symbols('t')
-res = integrate(-20*sin(t)*cos(t) - 16*sin(t)**2 + 8*cos(t)**2, (t, 0, pi/4))
-print(res)
+import numpy as np
+import sympy as sp
+from scipy.constants import *
 
