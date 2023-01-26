537e125127f0e8af92e8db5f8db4b8da22fbfef5
DarthBaguette <jlin5438@outlook.com>
2023-01-26 13:26:51 -0800

    Updated test.c with ordered linked list program

--- C&C++/test.c
+++ C&C++/test.c
@@ -1,14 +1,107 @@
 #include <stdio.h>
-#include <string.h>
-#include "cs50.h"
+#include <stdlib.h>
 
-int main(void)
+typedef struct node
 {
-    float T1 = -196, T2 = 36.1, p1 = 0.352, p2 = 1.454;
+    int number;
+    struct node *next;
+}
+node;
 
-    float B = (p2 - p1) / (T2 - T1);
-    float A = p2 - B * T2;
+node* create_node(int number);
+void place_node(node *list, node *n);
+void print_list(node *list);
+void free_list(node *list);
 
-    printf("P = %f + (%f)T\n", A, B);
+int main(int argc, char **argv)
+{
+    // Memory for numbers
+    node *list = NULL;
 
+    // For each CLI argument
+    for (int i = 1; i < argc; i++)
+    {
+        // Convert argument to int
+        int number = atoi(argv[i]);
+
+        // Allocate node for number
+        node *n = create_node(number);
+		if (n == NULL) { return 1; }
+
+        // If list is empty
+        if (list == NULL)
+        {
+            list = n;
+        }
+        // If number belongs at beginning of list
+        else if (n->number < list->number)
+        {
+            n->next = list;
+            list = n;
+        }
+        // If number belongs later in list
+        else
+        {
+            place_node(list, n);
+        }
+    }
+
+    // Print numbers
+    print_list(list);
+
+    // Free memory
+    free_list(list);
+}
+
+node* create_node(int number)
+{
+	node *n = malloc(sizeof(node));
+	if (n == NULL)
+	{
+		return NULL;
+	}
+	n->number = number;
+	n->next = NULL;
+
+	return n;
+}
+
+void place_node(node *list, node *n)
+{
+	// Iterate over nodes in list
+	for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+	{
+		// If at end of list
+		if (ptr->next == NULL)
+		{
+			ptr->next = n;
+			return;
+		}
+		// If in middle of list
+		if (n->number < ptr->next->number)
+		{
+			n->next = ptr->next;
+			ptr->next = n;
+			return;
+		}
+	}
+}
+
+void print_list(node *list)
+{
+	for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i\n", ptr->number);
+    }
+}
+
+void free_list(node *list)
+{
+	node *ptr = list;
+    while (ptr != NULL)
+    {
+        node *tmp = ptr->next;
+        free(ptr);
+        ptr = tmp;
+    }
 }
